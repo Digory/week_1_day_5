2b11dd40e36cd2975f006d0aad8001bae7f04072
Digory <digory1@googlemail.com>
2018-06-29 11:28:28 +0100

    test_sell_pet_to_customer__pet_not_found passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -70,8 +70,10 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
+  if pet_shop[:pets].include?(pet)
   add_pet_to_customer(customer, pet)
   increase_pets_sold(pet_shop, 1)
   remove_customer_cash(customer, pet[:price])
   add_or_remove_cash(pet_shop, pet[:price])
+end
 end
